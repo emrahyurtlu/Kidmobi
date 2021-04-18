0f4e5fbc6eb05038b8babd16dfe0a0e779a54632
Emrah Yurtlu <emrahyurtlu@gmail.com>
2021-04-19 02:28:29 +0300

    Device settings are changed successfully!

--- app/src/main/java/com/kidmobi/assets/service/RemoteSettingsService.kt
+++ app/src/main/java/com/kidmobi/assets/service/RemoteSettingsService.kt
@@ -4,9 +4,11 @@ import android.app.Service
 import android.content.Intent
 import android.os.IBinder
 import com.google.firebase.firestore.FirebaseFirestore
+import com.kidmobi.assets.enums.DbCollection
 import com.kidmobi.assets.repositories.MobileDeviceRepo
 import com.kidmobi.assets.utils.SettingsUtil
 import com.kidmobi.assets.utils.SharedPrefsUtil
+import com.kidmobi.assets.utils.toMobileDevice
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -17,6 +19,7 @@ class RemoteSettingsService : Service() {
     lateinit var settingsUtil: SettingsUtil
     lateinit var sharedPrefsUtil: SharedPrefsUtil
     lateinit var repo: MobileDeviceRepo
+    lateinit var db: FirebaseFirestore
 
     override fun onBind(intent: Intent?): IBinder? {
         Timber.d("onBind is started!")
@@ -28,6 +31,7 @@ class RemoteSettingsService : Service() {
         settingsUtil = SettingsUtil(this)
         sharedPrefsUtil = SharedPrefsUtil(this)
         repo = MobileDeviceRepo(FirebaseFirestore.getInstance())
+        db = FirebaseFirestore.getInstance()
         Timber.d("onCreate is started!")
     }
 
@@ -35,10 +39,24 @@ class RemoteSettingsService : Service() {
         Timber.d("onStartCommand is started!")
         val deviceId = sharedPrefsUtil.getDeviceId()
         CoroutineScope(Dispatchers.Main).launch {
-            val device = repo.getById(deviceId)
-            Timber.d("$device")
-            settingsUtil.changeDeviceSound(device.settings.soundLevel.toInt())
-            settingsUtil.changeScreenBrightness(device.settings.brightnessLevel.toInt())
+            //val device = repo.getById(deviceId)
+            db.collection(DbCollection.MobileDevices.name).document(deviceId).addSnapshotListener { snapshot, e ->
+                if (e != null) {
+                    Timber.w("Listen failed: ${e.message}")
+                    return@addSnapshotListener
+                }
+
+                if (snapshot != null && snapshot.exists()) {
+                    Timber.d("Current data: ${snapshot.data}")
+                    val device = snapshot.toMobileDevice()
+                    Timber.d("$device")
+                    settingsUtil.changeDeviceSound(device.settings.soundLevel.toInt())
+                    settingsUtil.changeScreenBrightness(device.settings.brightnessLevel.toInt())
+                } else {
+                    Timber.d("Current data: null")
+                }
+            }
+
         }
 
 
